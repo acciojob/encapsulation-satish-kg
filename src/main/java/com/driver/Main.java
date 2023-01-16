2a92e952a6afa5030a1561b96e40e7f8f1f3308f
Satish <satishkrgupta555@gmail.com>
2023-01-16 16:33:41 +0530

    encapsulation v1.0

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main (String[] args){
+        RWOnly newrw = new RWOnly();
+        newrw.setName("NewName");
+//        System.out.println(newrw.getName());
+        newrw.getName();
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void RWOnly(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
